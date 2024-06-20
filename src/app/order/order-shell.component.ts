ee4206ce8c4fef6ac8b5a3e8ed88091fcebc3d01
Azumara Joseph <azumarajoseph@gmail.com>
2024-06-20 11:35:36 +0100

    wooow

--- src/app/order/order-shell.component.ts
+++ src/app/order/order-shell.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-order-shell',
   template: `
-  <div class="row justify-content-center" style="background-color: rgba(246, 246, 246, 0.982); margin-top: 4rem;" >
-    <div class="col-md-7">
+  <div class="row justify-content-center" style="background-color: rgba(246, 246, 246, 0.982); margin-top: 4rem; padding: 0 2rem" >
+    <div class="col-md-8">
       <app-create-order></app-create-order>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
       <app-create-order-total></app-create-order-total>
     </div>
   </div>
